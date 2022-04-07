983c3cf905d183f9bec8e0db3e5c9675258d507b
akibhasan50 <tonuakibhasan@gmail.com>
2022-04-07 23:47:27 +0600

    resolve 2

--- src/utils/http-common.ts
+++ src/utils/http-common.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 export default axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:8000/api",
-=======
   baseURL: "http://localhost:8080/api",
->>>>>>> feature
   headers: {
     "Content-type": "application/json",
   },
