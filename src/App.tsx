6a2c2ff7b48f7ce70bfdc2f4ffc00775fd374b14
akibhasan50 <tonuakibhasan@gmail.com>
2022-10-13 00:48:04 +0600

    dashboard navigation bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 
+import { useMatch, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 function App() {
-  console.log(process.env)
+  const navigate = useNavigate();
+const dashboardRoute = useMatch("/");
+useEffect(() => {
+  if (dashboardRoute) {
+    navigate("/dashboard");
+  }
+}, [dashboardRoute, navigate]);
   return (
     <>
       <AppRoutes></AppRoutes>
